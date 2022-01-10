299383f1e0d8f44a36a57c3a15287061c5c6828e
Ivan <IvanIsakov568@yandex.ru>
2022-01-10 23:02:37 +0300

    Новая карта

--- data/desert_map/PineTrees.tsx
+++ data/desert_map/PineTrees.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="PineTrees" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="PineTrees.png" width="96" height="32"/>
+ <image source="../winter_map/PineTrees.png" width="96" height="32"/>
 </tileset>
